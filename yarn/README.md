3b2a2dddef1255b7051c3167c4a435ef55fe0226
macky <mackybeltran1@gmail.com>
2017-03-25 02:38:29 +0000

    adding more seeds

--- yarn/README.md
+++ yarn/README.md
@@ -4,6 +4,6 @@ Database = postgresql
 
 Run bundle install to install all the necessary gems
 
-Run bin/rails db:migrate to run the database migration
+Run bin/rake db:reset to run the database migration
 
 Run bin/rails s -b 0.0.0.0 to start the server on vagrant
--- yarn/db/seeds.rb
+++ yarn/db/seeds.rb
@@ -3,6 +3,10 @@ Panel.create(story_id: 1,
              body_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tristique sollicitudin luctus. Ut diam libero, maximus vitae consequat at, tristique sodales massa. Vestibulum viverra tincidunt dui, vitae ultricies diam sodales varius. Phasellus blandit tortor nisi, in dapibus leo hendrerit in.",
              player_id: 1)
 
+Panel.create(story_id: 2,
+             image: "http://www.myabandonware.com/media/screenshots/j/james-clavells-shogun-gp/james-clavells-shogun_7.gif",
+             body_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tristique sollicitudin luctus. Ut diam libero, maximus vitae consequat at, tristique sodales massa. Vestibulum viverra tincidunt dui, vitae ultricies diam sodales varius. Phasellus blandit tortor nisi, in dapibus leo hendrerit in.",
+             player_id: 1)
 
 User.create(username: "test",
             email: "test@test.com",
@@ -10,12 +14,21 @@ User.create(username: "test",
             avatar_url: "https://static.comicvine.com/uploads/original/4/46347/1889307-fafhrd.jpg"
             )
 
+User.create(username: "example",
+            email: "example@example.com",
+            password: "example",
+            avatar_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzqZc8Uo9cu7fyLHzuBXv-U17svUx8YNYvhz_LT_yZIzebV-Bf"
+            )
 
 Story.create(user_id: 1,
              title: "Fahfrd and the Grey Mouser",
              tagline: "Classic Adventure!",
              number_of_players: 2)
 
+Story.create(user_id: 2,
+             title: "Shogun",
+             tagline: "Battle for Japan",
+             number_of_players: 1)
 
 Player.create(story_id: 1,
               art: "http://piq.codeus.net/static/media/userpics/piq_157557_400x400.png",
@@ -23,11 +36,15 @@ Player.create(story_id: 1,
               name: "Fahfrd")
 
 
-Player.create(story_id: 2,
+Player.create(story_id: 1,
               art: "https://opengameart.org/sites/default/files/idle_11.gif",
               player_ordinal_number: 2,
               name: "Grey Mouser")
 
+Player.create(story_id: 2,
+              art: "http://www.coverbrowser.com/image/misc-games/3956-1.jpg",
+              player_ordinal_number: 1,
+              name: "Anjin-san")
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
