e695fa17918f25609581bdb960f79eb00edba548
Macky Beltran <mackybeltran1@gmail.com>
2017-03-26 13:39:39 -0400

    added some validations in the models

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,4 +4,7 @@ class Player < ApplicationRecord
   has_many :panels
   has_many :choices
 
+  validates :story_id, presence: true
+  validates :player_ordinal_number, presence: true
+  validates :name, length: {maximum: 30}
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -4,4 +4,9 @@ class Story < ApplicationRecord
   has_many :panels
   has_many :players
 
+  validates :title, :number_of_players, presence: true
+  validates :title, length: { maximum: 75 }
+  validates :tagline, length: { maximum: 140 }
+  validates :number_of_players, numericality: { less_than: 5}
+
 end
