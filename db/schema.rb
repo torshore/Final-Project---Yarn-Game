d1a7659b6180c8a9f267de7719fadd12cb605bb0
Lucy Wang <LucyTheGreat@Lucys-MacBook-Air.local>
2017-04-13 15:20:21 -0400

    database updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405151425) do
+ActiveRecord::Schema.define(version: 20170413191447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "choices", force: :cascade do |t|
+    t.integer  "panels_id"
+    t.integer  "stories_id"
     t.integer  "panel_id"
     t.integer  "path_to"
     t.string   "body_text"
@@ -27,16 +29,19 @@ ActiveRecord::Schema.define(version: 20170405151425) do
     t.integer  "index2"
     t.text     "image"
     t.string   "panel_text"
+    t.index ["panels_id"], name: "index_choices_on_panels_id", using: :btree
+    t.index ["stories_id"], name: "index_choices_on_stories_id", using: :btree
   end
 
   create_table "panels", force: :cascade do |t|
     t.integer  "story_id"
-    t.text     "image"
     t.string   "body_text"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "panel_title"
     t.integer  "index"
+    t.text     "image"
+    t.index ["story_id"], name: "index_panels_on_story_id", using: :btree
   end
 
   create_table "stories", force: :cascade do |t|
